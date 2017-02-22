83a7098035f6582123cc5f37009af6a21682afe9
swang0926 <swang926@mail.ustc.edu.cn>
2017-02-22 21:21:05 +0800

    2015ICCV flownet_tensorflow
    no transformer layer

---
+++ config.py
@@ -0,0 +1,12 @@
+FILE_PATH = '/data/FlyingChairs/data'
+LIST_PATH = '/data/list'
+
+BATCH_SIZE = 4
+IMAGE_HEIGHT = 384
+IMAGE_WIDTH = 512
+IMAGE_CHANNELS = 3
+FLO_CHANNELS = 2
+
+INITIAL_LEARNING_RATE = 1e-4
+LOG_DIR = '/home/pyramid/flownet/'
+MAX_STEPS = 1200000
